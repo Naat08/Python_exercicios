ee072bce82453b4a62d0b523115a252b2abe5f30
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-07 10:56:47 -0300

    Create exercicios_3.py

---
+++ Entrada_saida/exercicios_3.py
@@ -0,0 +1,5 @@
+print("Bem-vindo(a) às aulas de geometria plana online, aqui vamos calcular a Área do Triângulo!")
+base=float(input("Informe o valor da base: "))
+alt=float(input("Informe o valor da altuara: "))
+cal=(base+alt)/2
+print(f"O valor da área do triângulo é {cal}")

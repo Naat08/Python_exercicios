4df4e538259a1c70a1106340c4d9a55eef798934
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-07 11:16:38 -0300

    exercicio 4

---
+++ Entrada_saida/exercicio_4.py
@@ -0,0 +1,6 @@
+print("Insira suas notas para calcularmos sua média artmética desta etapa.")
+n1=float(input("Informe a primeira nota: "))
+n2=float(input("Informe a segundo nota: "))
+n3=float(input("Informe a terceira nota: "))
+media=(n1+n2+n3)/3
+print(f"Sua média aritmética é {media}")

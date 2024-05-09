4d60c91ff3dd4e4e14cd5b9825c01b8a3cf37f6a
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-09 11:48:42 -0300

    viacep - correção

--- condicionais/exercicio_1.py
+++ condicionais/exercicio_1.py
@@ -1,1 +1,8 @@
-cep=str(input('Digite seu CEP: '))
+import json
+import urllib.request
+
+# Solicitar ao usuário o CEP
+cep = input("Digite o CEP: ")
+
+# Construir na URL de consulta
+url = f"https://viacep.com.br/ws/{cep}/json/"

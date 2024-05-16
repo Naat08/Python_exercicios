28c226e0a0af0979c7a6f78c0fe4b679099ba511
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-16 08:21:15 -0300

    Update desafio.py

--- condicionais/desafio.py
+++ condicionais/desafio.py
@@ -1,5 +1,5 @@
 print("Nossa empresa presta serviço á companhia de energia elétrica do estado, nós auxiliamos nos cálculos das principais grandezas da eletricidades que são Tensão, Resistência e Corrente.")
-r1=str(input("Você deseja calcular a Tensão (digite U), a Resistência (digite R)ou a Corrente (digite i): "))
+r1=str(input("Você deseja calcular a Tensão (digite U), a Resistência (digite R)ou a Corrente (digite i)?: "))
 if (r1=="U") or (r1=="u"):
     res1=float(input("Digite o valor valor da Resistência: "))
     cor1=float(input("Digite o valor da Corrente: "))
@@ -17,5 +17,3 @@ elif (r1=="R") or (r1=="r"):
     print(f"O valor da Corrente é {v3}")
 else:
     print("Digite um valor válido!")
-
-

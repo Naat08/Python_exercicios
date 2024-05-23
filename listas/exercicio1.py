72d26c4c3a309b0ad54d9d10a780e8931ec67fc4
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-23 12:09:54 -0300

    Update exercicio1.py

--- listas/exercicio1.py
+++ listas/exercicio1.py
@@ -1,8 +1,8 @@
-numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-dobro = []
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+dobros = []
 
 for numero in numeros:
-    dobro.append(numero*2)
+    dobros.append(numero*2)
 
 print(numeros)
-print(dobro)
+print(dobros)

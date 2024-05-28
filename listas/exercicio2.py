29f08bdc01eb7213e3651226a95405830ea8d38b
Naat08 <ribeirodesouzanatalia16@gmail.com>
2024-05-28 09:04:38 -0300

    final exercicio2 - listas

--- listas/exercicio2.py
+++ listas/exercicio2.py
@@ -1,7 +1,12 @@
 numeros = []
-quant = 5
+quant = 1
 
 while quant <= 5:
-    digit=int(input("Digite 5 números inteiros: "))
-numeros.append(digit)
-print(numeros)
+    digit=int(input(f"Digite {quant}º números inteiros: "))
+    numeros.append(digit)
+    quant+=1
+print(f"Sua lista: {numeros}")
+print(f"A soma da sua lista: {sum(numeros)}")
+print(f"O maior número da sua lista: {max(numeros)}")
+print(f"O menor número da sua lista: {min(numeros)}")
+print(f"A média dos números é: {sum(numeros)/len(numeros)}")
